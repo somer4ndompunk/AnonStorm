7e2c94a9392c9a748cc000c4d800f89fae4b1030
somer4ndompunk <somer4ndompunk@gmail.com>
2013-05-03 19:57:14 -0400

    first commit

--- README.md
+++ README.md
@@ -9,11 +9,13 @@ Prophet and kyzersane for the input and initial guidance in the project
 **********************
 *Requirements & Setup*
 **********************
+
      Python, Python-Twitter, Python-pip, OAuth2, simplejson, httplib2
 
 ********
 *Debian*
 ********
+
    sudo apt-get install python
    sudo apt-get install python-pip
    sudo pip install python-twitter
@@ -21,6 +23,7 @@ Prophet and kyzersane for the input and initial guidance in the project
 *********
 *Windows*
 *********
+
 Replace XX with your python version number. Example: Python10, Python27, Python31, etc
 
    reference: https://python-guide.readthedocs.org/en/latest/starting/install/win.html
@@ -41,15 +44,18 @@ Install dependencies
 
    execute pip install python-twitter
    End result -> "Successfully installed python-twitter simplejson oauth2 httplib2"
-                                *****
-                                *Mac*
-                                *****
+
+*****
+*Mac*
+*****
+
    sudo easy_install pip
    sudo pip install python-twitter
 
 *******
 *Usage*
 *******
+
 You MUST run auth.py prior to running the storm.py script.
 Simply run "python auth.py" and follow the onscreen instructions.
 The end result is you will now have an Access Token Key and an Access Token Secret, be sure to save these somewhere safe for future use.
@@ -64,17 +70,21 @@ This script can run with more than one instance. You will need to enter your Key
 *********************************
 *Setting a Static Key and Secret*
 *********************************
+
 If you so wish, you can save your Key and Secret within the script and not have to use the --access_token_secret and --access_token_key with each run.
+
 To do this, edit the storm.py file and locate lines 62 and 63, they look like:
 
     arg_parser.add_argument("--access_token_key",required=True,type=str,help="Twitter access token key (see file for instructions)")
     arg_parser.add_argument("--access_token_secret",required=True,type=str,help="Twitter access token secret (see file for instructions)")
 
 Simply add default="YOUR_KEY_HERE" on line 62 and
+
 default="YOUR_SECRET_HERE" on line 63, like so:
 
     arg_parser.add_argument("--access_token_key",default="ggadhfj543ddbvbt543",required=True,type=str,help="Twitter access token key (see file for instructions)")
     arg_parser.add_argument("--access_token_secret",default="gghcnryhghbvvc854336vjnhy6",required=True,type=str,help="Twitter access token secret (see file for instructions)")
 
 Then save the file.
+
 YOU WILL STILL RETAIN ABILITY TO OVERRIDE BY USING THE --access_token_key and --access_token_secret ARGUMENTS
--- storm.py
+++ storm.py
@@ -1,49 +1,68 @@
+	
+
 # AnonStorm twitterstorm bot
+#
+# Usage: python storm.py --access_token_key <twitter access_token_key> --access_token_secret <twitter access_token_secret>
+#                        --tweet_file_url <tweet_file_url>  (optional; if not used, will default to AnonStorm/TweetLog.txt)
 #
 # IF YOU RECEIVED THIS SCRIPT FROM ANOTHER LOCATION OTHER THAN GITHUB.COM/SOMER4NDOMPUNK THEN YOU WILL BE USING AN OUTDATED AND
 # POTENTIALLY DANGEROUS VERSION OF THIS SCRIPT. NEVER EVER EVER DOWNLOAD SHIT FROM A FILE HOST, YOU CAN NOT VERIFY THE CODE
 # UNTIL YOU DOWNLOAD IT, BY THEN, IT COULD BE TOO LATE!!
 #
-# This script, as it sits will pull from an online file.
-# It will pull a random tweet from the hosted file, and tweet every 2 minutes a new random tweet.
-# 
-# You MUST run the auth.py script first to obtain your access_token_key and your access_token_secret. This will authorize the app with your twitter account.
-# Once you verify the pin, you MUST update the appropriate strings access_token_key and access_token_secret with the key and secret auth.py gives you.
-#
 # A VERY special thanks to The, Happyface, Prophet and kyzersane for their work on this script, hosting the script, and hosting the tweet file.
 # Without each of your feed backs this project would never have become a reality.
 #
 # Version 1.0
-
-import time, datetime, sched, random
+ 
+import time, datetime, sched, random, argparse
 import urllib2
 import twitter
-     
-print('Welcome to AnonStorm v1.0 | If you did not download this from SomeR4ndomPunks GitHub then you are outdated and at risk! Enjoy')
-# Do not edit consumer_key nor consumer_secret. These are used by Twitters API
-api=twitter.Api(consumer_key='5VuZ39FbuRfFdr0CpNf3zg',consumer_secret='5FPi40pwS3buTMFMXm8URXoRSCubw0LqM5KtTPkdo',access_token_key='YOUR_ACCESS_TOKEN_HERE',access_token_secret='YOUR_ACCESS_SECRET_HERE')
-     
-contents=[]
-ts = time.time()
-st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-s  = sched.scheduler(time.time, time.sleep)
  
-def do_something(sc):
-        response = urllib2.urlopen('LINK_TO_OP_TWEET_FILE')
-        contents = response.readlines()
+def main(args):
+    print("Welcome to AnonStorm v1.0 | If you did not download this from SomeR4ndomPunks GitHub then you are outdated and at risk! Enjoy\n")
+    # Do not edit consumer_key nor consumer_secret. These are used by Twitters API
+    api=twitter.Api(consumer_key="5VuZ39FbuRfFdr0CpNf3zg",consumer_secret="5FPi40pwS3buTMFMXm8URXoRSCubw0LqM5KtTPkdo",access_token_key=args.access_token_key,access_token_secret=args.access_token_secret)
+         
+    contents=[]
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
+    s  = sched.scheduler(time.time, time.sleep)
+     
+    def do_something(sc):
+        if args.tweet_file_url:
+            try:
+                response = urllib2.urlopen(args.tweet_file_url)
+                contents = response.readlines()
+            except:
+                print("Error loading text file contents. Ensure http:// prepends your url.")
+        else:
+            try:
+                with open("TweetLog.txt") as f:
+                    contents = f.readlines()
+            except:
+                print("Looks like you moved TweetLog.txt and did not specify a tweet file URL with the --tweet_file_url option... Try again.")
+                return
  
         while True:
-                try:
-                        print("Tweeting...", st)
-                        print("", contents[random.randint(0,len(contents)-1)])
-                        status = api.PostUpdate(contents[random.randint(0,len(contents)-1)])
-                        break
-                except twitter.TwitterError as e:
-                        print("Twitter error received...", e.message)
-                        print("Trying again...")
+            try:
+                print("Tweeting...", st)
+                print("", contents[random.randint(0,len(contents)-1)])
+                status = api.PostUpdate(contents[random.randint(0,len(contents)-1)])
+                break
+            except twitter.TwitterError as e:
+                print("Twitter error received...", e.message)
+                print("Trying again...")
  
         sc.enter(120, 1, do_something, (sc,)) #Edit the "120" if you wish to speed or slow down the tweets. Time is in seconds. IE: 120 = 2 minutes
  
-s.enter(0, 1, do_something, (s,))
-s.run()
+    s.enter(0, 1, do_something, (s,))
+    s.run()
+ 
+if __name__=="__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--access_token_key",required=True,type=str,help="Twitter access token key (see file for instructions)")
+    arg_parser.add_argument("--access_token_secret",required=True,type=str,help="Twitter access token secret (see file for instructions)")
+    arg_parser.add_argument("--tweet_file_url",type=str,help="URL for new line delimited text file of tweets")
+    args = arg_parser.parse_args()
+    main(args)
 
